4b315689248a765b26ff0f1d0c08c294702228ea
Ali0369 <134069715+Ali0369@users.noreply.github.com>
2025-12-08 21:17:59 +0500

    Update forgot.js

--- forgot.js
+++ forgot.js
@@ -1,26 +1,3 @@
-// document.getElementById('forgotPasswordForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); 
-//     const email = document.getElementById('forgotEmail').value;
-//     const securityAnswerInput = document.getElementById('securityAnswerInput').value;
-//     const newPassword = document.getElementById('newPassword').value;
-//     const resetMessageDiv = document.getElementById('resetMessage');
-
-   
-//     const storedSecurityAnswer = localStorage.getItem(email + "_securityAnswer");
-
-   
-//     if (storedSecurityAnswer && storedSecurityAnswer === securityAnswerInput) {
-      
-//         localStorage.setItem(email + "_password", newPassword);
-//         resetMessageDiv.innerHTML = "<p style='color: green;'>Password reset successful!</p>";
-//     } else {
-//         resetMessageDiv.innerHTML = "<p style='color: red;'>Invalid email or security answer.</p>";
-//     }
-    
-// });
-
-
-
 document.getElementById('forgotPasswordForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
@@ -48,6 +25,7 @@ document.getElementById('forgotPasswordForm').addEventListener('submit', async f
         resetMessageDiv.innerHTML = "<p style='color: red;'>Error connecting to server.</p>";
     }
 });
+
 
 
 
